d213e7c8b06d6bb009581f6cce6f98a9a58826cf
Dheerax <dheeraxgamerz@gmail.com>
2026-04-09 19:08:49 +0530

    Bump IP fetch timeout to 7 seconds

--- helpers.py
+++ helpers.py
@@ -8,7 +8,7 @@ def fetch_location(ip):
     try:
         if ip in ('127.0.0.1', '::1', 'localhost'):
             return 'Local/Dev'
-        r = requests.get(f'http://ip-api.com/json/{ip}', timeout=3)
+        r = requests.get(f'http://ip-api.com/json/{ip}', timeout=7)
         data = r.json()
         if data.get('status') == 'success':
             city = data.get('city', '')
